ba7e78c0957c6147a0734b36ed59df0d805456f0
Manchalasarika <sarikamanchala33@gmail.com>
2021-11-10 20:04:16 -0600

    add

--- app.js
+++ app.js
@@ -1,43 +1,104 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var catRouter = require('./routes/cat');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+
+const connectionString = process.env.MONGO_CON;
+mongoose = require("mongoose");
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var catRouter = require("./routes/cat");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var addmodsRouter = require("./routes/addmods");
+var selectorRouter = require("./routes/selector");
+var resourceRouter = require('./routes/resource');
+var cat = require("./models/cat");
+
+// We can seed the collection if needed on server start
+async function recreateDB() {
+  // Delete everything
+  await cat.deleteMany();
+
+  let instance1 = new cat({
+    Brand: "Usha",
+    price: 10,
+    color: "Green",
+  });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved");
+  });
+  let instance2 = new cat({
+    Brand: "tas",
+    price: 20,
+    color: "red",
+  });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("second object saved");
+  });
+  let instance3 = new cat({
+    Brand: "pot",
+    price: 30,
+    color: "white",
+  });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("third object saved");
+  });
+}
+
+let reseed = true;
+if (reseed) {
+  recreateDB();
+}
+
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/selector', selectorRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+});
+
+app.use("/", indexRouter);
 app.use('/cat', catRouter);
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/addmods', addmodsRouter);
+app.use("/users", usersRouter);
+app.use("/selector", selectorRouter);
+app.use("/addmods", addmodsRouter);
+//app.use("/costume", costumeRouter);
+app.use('/resource', resourceRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
