1c604a8593e52815e5bff0dccb58e5f48542e05b
Manchalasarika <sarikamanchala33@gmail.com>
2021-11-18 23:12:38 -0600

    add

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 mongoose = require("mongoose");
 const connectionString = 'mongodb+srv://manchalaUser:lash5388@cluster0.tkcue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(connectionString, {
@@ -69,6 +83,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -83,7 +104,10 @@ app.use("/selector", selectorRouter);
 app.use("/addmods", addmodsRouter);
 //app.use("/costume", costumeRouter);
 app.use('/resource', resourceRouter);
-
+var Account =require('./models/account'); 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
